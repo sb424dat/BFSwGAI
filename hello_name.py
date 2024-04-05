ab596246935d7897865d413a42f0722be504e689
fcastle3 <fcastle@gatech.edu>
2024-04-05 12:36:14 -0500

    working code cleaned up

--- hello_name.py
+++ hello_name.py
@@ -1,10 +1,16 @@
 #hello_name
 # Python
-def greet_user(name):
-    print("Hello", name)
-
-name: str = input("Please enter your name: ")
-greet_user(name)
+def greet_user(name: str) -> None:
+    #A
+    #A
+    #A
+    """
+    Prints a greeting using the provided name.
+    Args:
+    - name (str): The name to be used in the greeting.
+    """
+    print(f"Hello, {name}!")
 
 #check for change
-
+name: str = input("Please enter a name: ")
+greet_user(name)
